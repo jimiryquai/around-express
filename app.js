0fe8022e2f947b198c79e6291f9a37dcac54fb8a
James Ryan <james.ryan@pathwayforcare.com>
2021-04-27 20:04:55 +0100

    Chnaged path

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const path = require('path');
-const cards = require('./routes/cards');
 const users = require('./routes/users');
+const cards = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, './public')));
-app.use('/', cards);
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', users);
+app.use('/', cards);
 
 app.get('*', (req, res) => {
   res.status(404).send({ message: 'Requested resource not found' });
