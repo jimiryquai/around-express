938a4562ddda60e06ccaa7988ff5d0172101e7e3
James Ryan <james.ryan@pathwayforcare.com>
2021-05-09 12:13:02 +0100

    Added controllers for cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,6 +2,8 @@ const Card = require('../models/card');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
+    .populate('owner')
+    .populate('likes')
     .then((cards) => {
       res.send({ data: cards });
     })
@@ -15,13 +17,31 @@ module.exports.createCard = (req, res) => {
 
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
-    .catch(() => res.status(400).send({ message: 'Bad Request' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Card Validation Failed' });
+      } else {
+        res.status(500).send({ message: 'Internal Server Error' });
+      }
+    });
 };
 
 module.exports.deleteCard = (req, res) => {
-  Card.findByIdAndRemove(req.params.id)
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(404).send({ message: 'Card Not Found' }));
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => {
+      if (card) {
+        res.send({ data: card });
+      } else {
+        res.status(404).send({ message: 'Card Not Found' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Invalid Card Id' });
+      } else {
+        res.status(500).send({ message: 'Internal Server Error' });
+      }
+    });
 };
 
 module.exports.likeCard = (req, res) => {
@@ -31,16 +51,41 @@ module.exports.likeCard = (req, res) => {
     { new: true },
   )
     .populate('likes')
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(404).send({ message: 'Card Not Found' }));
+    .then((card) => {
+      if (card) {
+        res.send({ data: card });
+      } else {
+        res.status(404).send({ message: 'Card Not Found' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Invalid Card Id' });
+      } else {
+        res.status(500).send({ message: 'Internal Server Error' });
+      }
+    });
 };
 
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
-    { $pull: { likes: req.user._id } }, // remove _id from the array
+    { $pull: { likes: req.user._id } },
     { new: true },
-  ).populate('likes')
-    .then((card) => res.send({ data: card }))
-    .catch(() => res.status(404).send({ message: 'Card Not Found' }));
+  )
+    .populate('likes')
+    .then((card) => {
+      if (card) {
+        res.send({ data: card });
+      } else {
+        res.status(404).send({ message: 'Card Not Found' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Invalid Card Id' });
+      } else {
+        res.status(500).send({ message: 'Internal Server Error' });
+      }
+    });
 };
--- routes/cards.js
+++ routes/cards.js
@@ -15,6 +15,6 @@ router.delete('/:cardId', deleteCard);
 
 router.put('/:cardId/likes', likeCard);
 
-router.put('/:cardId/likes', dislikeCard);
+router.delete('/:cardId/likes', dislikeCard);
 
 module.exports = router;
